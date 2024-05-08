beb3f8c5cecbbc3206a08ad0487306e3ed1132bf
celiob_ <celiolimabenjamim@gmail.com>
2024-05-07 22:33:10 -0400

    desafio

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        
+        @SuppressWarnings("resource")
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Cadastro do banco");
+        System.out.println("Por favor, digite o numero da sua agencia: ");
+        int numero = scanner.nextInt(); 
+
+        System.out.println("Digite sua agencia: ");
+        String agencia = scanner.next(); 
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next(); 
+
+        System.out.println("Insira seu saldo: ");
+        float saldo = scanner.nextFloat(); 
+
+
+        System.out.println("Olá "+nome+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");    
+    }
+}
